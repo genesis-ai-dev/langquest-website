056f0fbb3925134f5e8567d6ea0282b6a5e7aa7c
CalJosKos <120157396+CalJosKos@users.noreply.github.com>
2025-02-18 16:18:58 -0800

    test change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       
       <div>
         <h1 className="text-5xl font-light mb-5">
-          Future home of something quite cool.
+          Future home of something quite cool, in next.js.
         </h1>
         <p className="text-xl font-light mb-5">
           If you&apos;re the <strong>site owner</strong>, <a href="/cpanel" className="text-blue-500">log in</a> to launch this site
