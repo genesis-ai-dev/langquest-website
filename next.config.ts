ad4417dc8780d89ad89895e48bd6a2fdc7465e30
CalJosKos <120157396+CalJosKos@users.noreply.github.com>
2025-02-18 17:00:04 -0800

    add /out, adjust .cpanel.yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Creates a static export
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  trailingSlash: true, // Adds trailing slashes to URLs
 };
 
 export default nextConfig;
