8ef1eca5890ae6e95b9ae7ada1f39a3ab8df4d3d
keeandev <38674879+keeandev@users.noreply.github.com>
2025-04-22 12:46:11 -0500

    feat: posthog reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,19 @@ import '@/lib/env';
 const nextConfig: NextConfig = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
+  rewrites: async () => {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*'
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*'
+      }
+    ];
+  }
 };
 
 export default withGTConfig(nextConfig, {
