d18ea95c5b22870509794f6a18d95f357af71929
keeandev <38674879+keeandev@users.noreply.github.com>
2026-07-06 10:39:58 -0500

    update privacy policy

--- next.config.ts
+++ next.config.ts
@@ -9,23 +9,12 @@ const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
   async rewrites() {
     return [
-      /** Remove ingest routes after migration to relay endpoints in middleware.ts */
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*'
-      },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*'
-      },
       {
         source: '/supabase/:project/:path*',
         destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/:path*`
       }
     ];
   },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true
 };
 
 const withNextIntl = createNextIntlPlugin();
